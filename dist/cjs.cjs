378b3838cd99c70644857b148eaeb974ee05ad7e
frnAlt <sultana01537118@gmail.com>
2026-08-20 06:44:29 +0000

    feat(fca): unify into full hybrid engine with auto-extending login and direct exports

--- dist/cjs.cjs
+++ dist/cjs.cjs
@@ -1,9 +1,75 @@
 "use strict";
+const path = require("path");
 const m = require("./index.js");
-const core = typeof m.login === "function" ? m.login : m.default;
-if (typeof core !== "function") {
-  throw new Error("@dongdev/fca-unofficial: expected login to be a function (check dist/index.js exports).");
+
+let extendFCA;
+try {
+  extendFCA = require(path.join(__dirname, "../../func/fcaExtension.js"));
+} catch {
+  try {
+    extendFCA = require("../../func/fcaExtension.js");
+  } catch {
+    extendFCA = (api) => api;
+  }
 }
-Object.assign(core, m);
-core.default = core;
-module.exports = core;
+
+const originalLogin = typeof m.login === "function" ? m.login : m.default;
+if (typeof originalLogin !== "function") {
+  throw new Error("@floppa/fca-native: expected login to be a function (check dist/index.js exports).");
+}
+
+function hybridLogin(credentials, options, callback) {
+  let opts = options;
+  let cb = callback;
+
+  if (typeof opts === "function") {
+    cb = opts;
+    opts = {};
+  }
+
+  if (typeof cb === "function") {
+    return originalLogin(credentials, opts, (err, api) => {
+      if (!err && api) {
+        try {
+          extendFCA(api);
+        } catch (e) {
+          console.warn("[HybridFCA Warning extending API]:", e.message);
+        }
+      }
+      cb(err, api);
+    });
+  }
+
+  const res = originalLogin(credentials, opts);
+  if (res && typeof res.then === "function") {
+    return res.then(ctxOrApi => {
+      const target = ctxOrApi?.api || ctxOrApi;
+      if (target) {
+        try {
+          extendFCA(target);
+        } catch (e) {
+          console.warn("[HybridFCA Warning extending API]:", e.message);
+        }
+      }
+      return ctxOrApi;
+    });
+  }
+  return res;
+}
+
+Object.assign(hybridLogin, m);
+if (extendFCA) {
+  hybridLogin.extendFCA = extendFCA;
+  hybridLogin.ConduitMessageBuilder = extendFCA.ConduitMessageBuilder;
+  hybridLogin.ConduitAttachmentBuilder = extendFCA.ConduitAttachmentBuilder;
+  hybridLogin.ConduitMessageCollector = extendFCA.ConduitMessageCollector;
+  hybridLogin.ConduitSlidingCache = extendFCA.ConduitSlidingCache;
+  hybridLogin.ConduitQueue = extendFCA.ConduitQueue;
+  hybridLogin.AxeraNotesAPI = extendFCA.AxeraNotesAPI;
+  hybridLogin.AxeraThemeAPI = extendFCA.AxeraThemeAPI;
+  hybridLogin.AxeraEmojiAPI = extendFCA.AxeraEmojiAPI;
+  hybridLogin.resolvePhotoUrl = extendFCA.resolvePhotoUrl;
+}
+
+hybridLogin.default = hybridLogin;
+module.exports = hybridLogin;
